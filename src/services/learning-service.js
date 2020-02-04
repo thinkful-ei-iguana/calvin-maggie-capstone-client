599b75e0d0e13961418b69f8ddc56a0fd81c86bb
therealmbittarelli <maggiekmcclellan@gmail.com>
2020-02-04 17:47:43 -0500

    get request on /learn working

--- src/services/learning-service.js
+++ src/services/learning-service.js
@@ -1,40 +1,66 @@
 import config from '../config'
 import TokenService from './token-service'
 
+
 // have not built this out yet, dont trust data here
 
 const LearningService = {
 
-  getWord() {
-    let authToken = TokenService.getAuthToken();
+  getWords: () => {
     return fetch(`${config.API_ENDPOINT}/language`, {
       method: 'GET',
       headers: {
         'content-type': 'application/json',
-        'authorization': `Bearer ${authToken}`
-
+        'authorization': `Bearer ${TokenService.getAuthToken()}`
       }
     })
-      .then(res =>
-        (!res.ok)
-          ? res.json().then(e => Promise.reject(e))
-          : res.json()
-      )
+      .then(res => res.json())
+      .then(data => {
+        console.log('data is', data);
+        this.setState({
+          words: data.words,
+          totalScore: data.language.total_score
+        })
+      }
+      );
   },
-  // postGuess({ username, password }) {
-  //   return fetch(`${config.API_ENDPOINT}/auth/token`, {
-  //     method: 'POST',
-  //     headers: {
-  //       'content-type': 'application/json',
-  //     },
-  //     body: JSON.stringify({ username, password }),
-  //   })
-  //     .then(res =>
-  //       (!res.ok)
-  //         ? res.json().then(err => Promise.reject(err))
-  //         : res.json()
-  //     )
-  // },
+
+  getWord: () => {
+    return fetch(`${config.API_ENDPOINT}/language/head`, {
+      method: 'GET',
+      headers: {
+        'content-type': 'application/json',
+        'authorization': `Bearer ${TokenService.getAuthToken()}`
+      }
+    })
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          currentWord: data.original,
+          nextWord: data.next,
+          correctCount: data.correct_count,
+          incorrectCount: data.incorrect_count,
+          totalScore: data.total_score
+        })
+
+      })
+  },
+
+  postGuess: (guess) => {
+    return fetch(`${config.API_ENDPOINT}/language/guess`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+        'Authorization': `Bearer ${TokenService.getAuthToken()}`
+      },
+      body: JSON.stringify(guess)
+    })
+      .then(res => {
+        console.log('res is', res);
+        res.json()
+      });
+
+  }
 
 }
 
