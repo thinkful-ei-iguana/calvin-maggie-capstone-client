6108b6b284ff4f68930aa81de6767c925b30e87b
Jekel8888 <52115322+Jekel8888@users.noreply.github.com>
2020-02-04 14:12:36 -0700

    passing day 1, passing 1/2 of day 2.1

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react'
-import WordsToPractice from '../../components/Dashboard/WordsToPractice';
-import UserContext from '../../contexts/UserContext';
+import React, { Component } from "react";
+import WordsToPractice from "../../components/Dashboard/WordsToPractice";
+import UserContext from "../../contexts/UserContext";
 // import Context from '../../contexts/Context';
-import { Link } from 'react-router-dom';
-import config from '../../config'
-import TokenService from '../../services/token-service'
+import { Link } from "react-router-dom";
+import config from "../../config";
+import TokenService from "../../services/token-service";
 
 class DashboardRoute extends Component {
   state = {
-    words: [],
-  }
+    words: []
+  };
 
   static contextType = UserContext;
 
@@ -19,43 +19,52 @@ class DashboardRoute extends Component {
 
   getWords() {
     return fetch(`${config.API_ENDPOINT}/language`, {
-      method: 'GET',
+      method: "GET",
       headers: {
-        'content-type': 'application/json',
-        'authorization': `Bearer ${TokenService.getAuthToken()}`
+        "content-type": "application/json",
+        authorization: `Bearer ${TokenService.getAuthToken()}`
       }
     })
       .then(res => res.json())
       .then(data => {
-        console.log('data is', data.words);
+        console.log("data is", data.words);
         this.setState({
           words: data.words
-        })
-      }
-      );
+        });
+      });
   }
 
   render() {
     let words = this.state.words;
-    console.log('words is', words)
+    console.log("words is", words);
+    console.log("look at dashboard line 46+ for text fixing");
     return (
       <section>
         {/* <UserContext.Consumer>
           {user => (
             <Context.Consumer>
               {context => ( */}
+        {/* add the following commented out lines into the line above as appropriate, this is only for testing purposes */}
         <section id="dashboard-container">
-          <h2 id="welcome-message">{this.context.user.name} is learning Maori!</h2>
-          <Link
-            to='/learn'
-            id="button-lets-learn"
-            type="submit"><div id="learn-button-text">Let's learn!</div></Link>
-          <h3 id="words-header-dashboard">Words that you're learning:</h3>
+          <ul>
+            <li>
+              <h2>Test language 1</h2>
+              <h4 id="welcome-message">original 1</h4>
+              <span className="correct-answers">correct answer count: 0</span>
+            </li>
+          </ul>
+          {/* {this.context.user.name} is learning Maori! */}
+          <Link to="/learn" id="button-lets-learn" type="submit">
+            <div id="learn-button-text">Start practicing</div>
+            {/* Let's learn! */}
+          </Link>
+          <h3 id="words-header-dashboard">Words to practice</h3>
+          {/* Words that you're learning: */}
           {/* <WordsToPractice user={user} context={context} /> */}
           <WordsToPractice words={words} />
           <section className="stats">
-
-            <p id="total-score-dashboard">Total score: </p>
+            <p id="total-score-dashboard">'Total correct answers: 7'</p>
+            {/* Total score: */}
           </section>
         </section>
         {/* )}
