aca1c06572ed306d7cc4978d531a326ab5926184
Jekel8888 <52115322+Jekel8888@users.noreply.github.com>
2020-02-09 21:05:47 -0700

    fixed requested revisions and redeployed, sent in for another grading round

---
+++ build/precache-manifest.aa3d9ce26b5289390ec1d82209f97bb1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "32729d9b4a113269bcd9",
+    "url": "/static/css/main.8d3472ba.chunk.css"
+  },
+  {
+    "revision": "32729d9b4a113269bcd9",
+    "url": "/static/js/main.32729d9b.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "78bd25c72a10afe5e32d",
+    "url": "/static/js/2.78bd25c7.chunk.js"
+  },
+  {
+    "revision": "07bc4093bd020c89d2a43d29c86181aa",
+    "url": "/index.html"
+  }
+];
